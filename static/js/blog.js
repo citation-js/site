60a1467805d8128ad8e255254c1600d45b044c5c
Lars Willighagen <lars.willighagen@gmail.com>
2018-08-06 20:27:21 +0200

    [site:blog] Add pagination, update styling

--- static/js/blog.js
+++ static/js/blog.js
@@ -1,10 +1,15 @@
-if (!Array.prototype.map) {
-  Array.prototype.map = function (mapper) {
-    let array = this
-    for (var i = 0; i < array.length; i++) {
-      array[i] = mapper.call(array[i], i, array)
+function map (array, mapper) {
+  for (var i = 0; i < array.length; i++) {
+    array[i] = mapper(array[i], i, array)
+  }
+  return array
+}
+
+function find (array, predicate) {
+  for (var i = 0; i < array.length; i++) {
+    if (predicate(array[i], i, array)) {
+      return array[i]
     }
-    return array
   }
 }
 
@@ -14,29 +19,34 @@ function formatDate (string) {
 }
 
 function canonical (links) {
-  for (var i = 0; i < links.length; i++) {
-    if (links[i].rel === 'alternate' && links[i].type === 'text/html') {
-      return links[i].href
-    }
-  }
+  return find(links, function (link) {
+    return link.rel === 'alternate' && link.type === 'text/html'
+  }).href
 }
 
 var templates = {
   feed: function (items) {
-    return '<div>' +
-      items.map(templates.feedItem).join('') +
-    '</div>'
+    return map(items, templates.feedItem).join('')
   },
   feedItem: function (item) {
     var id = item.id.$t.replace(/^.*\.post-(\d+)$/, '$1')
+    var content = item.content.$t
+      .replace(/^[\s\S]*<div class="stackedit__html">([\s\S]*)<\/div>[\s\S]*$/, '$1')
+      .replace(/<(\/?)h([1-6])/g, function (match, slash, level) {
+        if (+level > 4) {
+          return '<' + slash + 'b'
+        } else {
+          return '<' + slash + 'h' + (+level + 2)
+        }
+      })
 
-    return '<div class="mdl-card mdl-shadow--2dp">' +
+    return '<div class="mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col">' +
       '<div class="mdl-card__title">' +
         '<h2 class="mdl-card__title-text">' +
           '<a href="?post=' + id + '">' + item.title.$t + '</a>' +
         '</h2>' +
       '</div>' +
-      '<div class="header mdl-card__supporting-text">' +
+      '<div class="mdl-card__supporting-text mdl-card--border">' +
         '<p>' +
           '<span><i class="material-icons">edit</i> ' +
             templates.author(item.author[0]) +
@@ -50,12 +60,12 @@ var templates = {
         '</p>' +
         '<p>' +
           '<span><i class="material-icons">bookmark</i> ' +
-            item.category.map(templates.tag).join(' ') +
+            map(item.category, templates.tag).join(' ') +
           '</span>' +
         '</p>' +
       '</div>' +
       '<div class="mdl-card__supporting-text">' +
-        item.content.$t +
+        content +
       '</div>' +
     '</div>'
   },
@@ -82,6 +92,28 @@ var templates = {
 
 function cb (data) {
   content.innerHTML = data.feed ? templates.feed(data.feed.entry) : templates.feedItem(data.entry)
+
+  // pagination
+  if (data.feed) {
+    var href = location.href
+    var hasIndex = href.indexOf('start-index') > -1
+    var hasParams = href.indexOf('?') > -1
+    var indexPattern = /start-index=(\d+)/
+
+    var prev = find(data.feed.link, function (link) { return link.rel === 'previous' })
+    if (prev) {
+      prev = 'start-index=' + prev.href.match(indexPattern)[1]
+      var url = hasIndex ? href.replace(indexPattern, prev) : href + (hasParams ? '?' : '') + prev
+      paginatePrev.setAttribute('href', url)
+    }
+
+    var next = find(data.feed.link, function (link) { return link.rel === 'next' })
+    if (next) {
+      next = 'start-index=' + next.href.match(indexPattern)[1]
+      var url = hasIndex ? href.replace(indexPattern, next) : href + (hasParams ? '&' : '?') + next
+      paginateNext.setAttribute('href', url)
+    }
+  }
 }
 
 function load (url) {
@@ -96,11 +128,22 @@ window.onload = function () {
     params[pair[0]] = pair[1]
   })
 
+  var url
+
   if (params.post) {
-    load('https://larsgw.blogspot.com/feeds/posts/default/' + params.post + '?alt=json-in-script&callback=cb')
+    url = 'https://larsgw.blogspot.com/feeds/posts/default/' + params.post + '?alt=json-in-script&callback=cb'
   } else if (params.tag) {
-    load('https://larsgw.blogspot.com/feeds/posts/default/-/Citation.js/' + params.tag + '?alt=json-in-script&callback=cb')
+    url = 'https://larsgw.blogspot.com/feeds/posts/default/-/Citation.js/' + params.tag + '?alt=json-in-script&callback=cb'
+  } else if (params.query) {
+    url = 'https://larsgw.blogspot.com/feeds/posts/default/-/Citation.js/?q=' + params.query + '&alt=json-in-script&callback=cb'
   } else {
-    load('https://larsgw.blogspot.com/feeds/posts/default/-/Citation.js?alt=json-in-script&callback=cb')
+    url = 'https://larsgw.blogspot.com/feeds/posts/default/-/Citation.js?alt=json-in-script&callback=cb'
   }
+
+  var startIndex = location.href.match(/start-index=(\d+)/)
+  if (startIndex) {
+    url += '&' + startIndex[0]
+  }
+
+  load(url)
 }
