2bb93edb5db09338029e76c1ed8ba57f2df0d0dd
Lars Willighagen <lars.willighagen@gmail.com>
2021-03-26 13:46:10 +0100

    fix(404): remove redirect loop

--- static/js/redirect-docs.js
+++ static/js/redirect-docs.js
@@ -1,4 +1,4 @@
-var docsPattern = /(\/api)(\/|$)/
+var docsPattern = /(\/api)(\/(?!0.[35]\/)|$)/
 
 if (docsPattern.test(location.pathname)) {
   location.pathname = location.pathname.replace(docsPattern, '$1/0.3$2')
