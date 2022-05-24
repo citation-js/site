0e4415545423c4de7a97b1e005b6fbde9226397c
Lars Willighagen <lars.willighagen@gmail.com>
2022-05-24 16:28:07 +0200

    feat(demo): update style, interface

---
+++ static/js/demo.js
@@ -0,0 +1,115 @@
+$(window).on('load', function () {
+  var Cite = require('citation-js')
+
+  function getType (element) {
+    var type = Cite.plugins.input.type(element.textContent)
+    if (type.slice(-4) === 'json' || type.slice(-6) === 'object') {
+      return 'json'
+    } else if (type.includes('@bibtex') || type.includes('@biblatex')) {
+      return 'bib'
+    } else if (type.slice(-3) === 'url' || type.includes('api')) {
+      return 'url'
+    }
+  }
+
+  // Update
+  var data
+  var old = {}
+  function updateInput () {
+    var text = $('#input').text()
+    var options = { generateGraph: false }
+
+    var type = $('#input-type').val()
+    if (type) {
+      options.forceType = type
+    }
+
+    if (text !== old.text || type !== old.type) {
+      try {
+        data = Cite(text, options)
+        $('#input-error').text('')
+        updateOutput()
+      } catch (error) {
+        $('#input-error').text(error.message)
+      }
+      old = { text: text, type: type }
+    }
+  }
+
+  function updateOutput () {
+    var format = $('#format').val()
+    if (format === 'bibliography') {
+      $('#output').html(data.format(format, {
+        template: $('#style').val(),
+        lang: $('#lang').val(),
+        format: 'html'
+      }))
+      return
+    }
+
+    var type = $('#type').val()
+    if (type === 'object') {
+      var object = data.format(format, { type: type })
+      $('#output').html('<pre>' + JSON.stringify(object, null, 2) + '</pre>')
+    } else {
+      $('#output').html('<pre>' + data.format(format, {
+        type: type
+      }) + '</pre>')
+    }
+
+    // Highlight
+    if (type === 'object' || format === 'data') {
+      $('#output pre').attr('data-type', 'json')
+      $('#output pre').data('type', 'json')
+      $('#output pre').highlight()
+    } else if (format === 'bibtex' || format === 'biblatex') {
+      $('#output pre').attr('data-type', 'bib')
+      $('#output pre').data('type', 'bib')
+      $('#output pre').highlight()
+    }
+  }
+
+  // Display info
+  $('#info-version').html(Cite.version.cite)
+  $('#info-plugins').html(Cite.plugins.list().join(', '))
+  var types = Cite.plugins.input.list()
+  for (var i = 0; i < types.length; i++) {
+    var [scope, type] = types[i].split('/')
+    var optgroup
+    while (!(optgroup = $('#input-type optgroup[label="' + scope + '"]')).length) {
+      $('#input-type').append('<optgroup label="' + scope + '"></optgroup>')
+    }
+    optgroup.append('<option value="' + scope + '/' + type + '">' + type + '</option>')
+  }
+
+  // Highlight all code
+  $('#input').highlight()
+  $('#input').focus(function () {
+    $(this).find('span').each(function () {
+      $(this).after(this.textContent)
+      $(this).remove()
+    })
+  })
+  $('#input').blur(function () {
+    var type = getType(this)
+    $(this).attr('data-type', type)
+    $(this).data('type', type)
+    $(this).highlight()
+  })
+
+  // Interactive forms
+  $('#format').change(function () {
+    if ($(this).val() === 'bibliography') {
+      $('#fieldset-bibliography').show()
+      $('#fieldset-files').hide()
+    } else {
+      $('#fieldset-bibliography').hide()
+      $('#fieldset-files').show()
+    }
+  })
+
+  $('#input').blur(updateInput)
+  $('#input-type').change(updateInput)
+  $('#format, #type, #style, #lang').change(updateOutput)
+  updateInput()
+})
